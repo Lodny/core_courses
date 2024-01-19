7681cd029fcfdfe9f307b74c4e3eed714ebc8eeb
yj.ahn <drinkjuice@naver.com>
2024-01-19 12:39:39 +0900

    add FlutterLocalNotification

---
+++ lib/push/flutter_notification.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class FlutterLocalNotification {
+  FlutterLocalNotification._();
+
+  static FlutterLocalNotificationsPlugin plugin = FlutterLocalNotificationsPlugin();
+
+  static init() async {
+    AndroidInitializationSettings androidSettings = const AndroidInitializationSettings(
+        'mipmap/ic_launcher');
+
+    InitializationSettings initializationSettings = InitializationSettings(
+        android: androidSettings,
+    );
+
+    await plugin.initialize(initializationSettings);
+  }
+
+  static Future<void> showNotification() async {
+    const AndroidNotificationDetails details = AndroidNotificationDetails(
+        'channel id', 'channel name',
+      channelDescription: 'channel description',
+      importance: Importance.max,
+      priority: Priority.max,
+      showWhen: false,
+    );
+
+    const NotificationDetails notificationDetails = NotificationDetails(
+      android: details,
+    );
+
+    await plugin.show(0, 'test title', 'test body', notificationDetails);
+  }
+}
